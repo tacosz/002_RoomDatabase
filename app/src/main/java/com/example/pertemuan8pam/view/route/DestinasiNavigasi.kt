a945c2ae5b45523167c913650aa498d85cfd17c1
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 18:39:13 +0700

    menambahkan fungsi untuk route dan titleres

--- app/src/main/java/com/example/pertemuan8pam/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pertemuan8pam/view/route/DestinasiNavigasi.kt
@@ -1,4 +1,13 @@
 package com.example.pertemuan8pam.view.route
 
 interface DestinasiNavigasi {
+    /**
+     * Nama unik unt menentukan jalur composable
+     */
+    val route: String
+
+    /**
+     * String resource id yg berisi judul yg akan ditampilkan di layar halaman.
+     */
+    val titleRes: Int
 }
