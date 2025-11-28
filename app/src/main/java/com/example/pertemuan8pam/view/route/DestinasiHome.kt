7625ba6d469986c347114544753c3612fa0e79f4
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 18:37:00 +0700

    menambahkan fungsi override tetap untuk route dan titleRes

--- app/src/main/java/com/example/pertemuan8pam/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/pertemuan8pam/view/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.pertemuan8pam.view.route
 
-object DestinasiHome {
+import com.example.pertemuan8pam.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
