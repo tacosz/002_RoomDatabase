3e21214ff63f8c16dc278fc7734e3f380a7cf070
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 15:47:43 +0700

    membuat class container data app berisi fungsi private yang tetap berisi kontex

--- app/src/main/java/com/example/pertemuan8pam/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan8pam/repositori/ContainerApp.kt
@@ -1,5 +1,16 @@
 package com.example.pertemuan8pam.repositori
 
+import android.content.Context
+import com.example.pertemuan8pam.room.DatabaseSiswa
+
 interface ContainerApp{
     val repositoriSiswa : RepositoriSiswa
 }
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao())
+        }
+        }
