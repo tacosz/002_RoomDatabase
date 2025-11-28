e4ac7971ecfe10d32c43232bf2f1d45b7a96e6d0
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 19:39:05 +0700

    membuat fungsi dan data class

--- app/src/main/java/com/example/pertemuan8pam/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan8pam/viewmodel/HomeViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.pertemuan8pam.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan8pam.repositori.RepositoriSiswa
+import com.example.pertemuan8pam.room.Siswa
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
     companion object{
@@ -9,10 +16,13 @@ class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
     }
     val homeUiState StateFLow<HomeUiState> = repositoriSiswa.getAllSiswaStream
     ()
-    .filterNotNull()
+    .fillterNotNull()
     .map {HomeUiState(listSiswa = it.toList())}
     .stateIn(scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
         initialValue = HomeUiState())
 
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
