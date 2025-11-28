b428efce1de8bc0b0161d8c348b9b52a9798c775
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 15:33:04 +0700

    membuat fungsi dengan implementasi untuk database siswa

--- app/src/main/java/com/example/pertemuan8pam/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/pertemuan8pam/room/DatabaseSiswa.kt
@@ -1,6 +1,8 @@
 package com.example.pertemuan8pam.room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
@@ -10,5 +12,14 @@ abstract class DatabaseSiswa : RoomDatabase() {
     companion object{
         @Volatile
         private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(lock = this){
+                Room.databaseBuilder(
+                    context, klass = DatabaseSiswa::class.java,
+                    name = "siswa_database")
+                    .build().also {Instance=it}
+            })
+        }
     }
 }
