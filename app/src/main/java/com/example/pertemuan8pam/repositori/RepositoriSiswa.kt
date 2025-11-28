8ab1d73a3f38f2211372dc6a3fd6e17bcbefe22b
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 15:41:00 +0700

    membuat class offline repositori siswa berisi fungsi yang tetap

--- app/src/main/java/com/example/pertemuan8pam/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan8pam/repositori/RepositoriSiswa.kt
@@ -1,9 +1,17 @@
 package com.example.pertemuan8pam.repositori
 
 import com.example.pertemuan8pam.room.Siswa
+import com.example.pertemuan8pam.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa{
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa{
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 }
