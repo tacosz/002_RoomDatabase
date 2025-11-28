fcb65737ca436c95e62ffa86a0b37d95c66685e8
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 19:28:05 +0700

    membuat class unt home view model

--- app/src/main/java/com/example/pertemuan8pam/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan8pam/viewmodel/HomeViewModel.kt
@@ -1,5 +1,11 @@
 package com.example.pertemuan8pam.viewmodel
 
-class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): {
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8pam.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 
 }
