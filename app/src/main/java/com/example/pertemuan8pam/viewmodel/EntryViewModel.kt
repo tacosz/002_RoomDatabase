9fcf7f81107a5615b37bc9bf108da7cb4566bc37
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 18:42:10 +0700

    fungsi yg berisi status siswa saat ini

--- app/src/main/java/com/example/pertemuan8pam/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan8pam/viewmodel/EntryViewModel.kt
@@ -1,2 +1,13 @@
 package com.example.pertemuan8pam.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8pam.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel()
+{
+    /**
+     * Berisi status siswa saat ini
+     */
+    var uiStatSiswa by mutableListOf(UIStateSiswa())
+        private set
+}
