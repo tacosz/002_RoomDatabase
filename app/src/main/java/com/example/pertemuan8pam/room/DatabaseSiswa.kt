2db10054297c32bf64856bc1540d7c0b38258286
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 15:29:40 +0700

    membuat companion object dan fungsi instance yang dapat berubah nilainya

--- app/src/main/java/com/example/pertemuan8pam/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/pertemuan8pam/room/DatabaseSiswa.kt
@@ -6,4 +6,9 @@ import androidx.room.RoomDatabase
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase() {
     abstract fun siswaDao() : SiswaDao
+
+    companion object{
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+    }
 }
