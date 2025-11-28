f63e4168317204ba628199cac5b8bd93eda300b6
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 15:28:19 +0700

    membuat abstract class db siswa dan fungsi abstract siswa dao

--- app/src/main/java/com/example/pertemuan8pam/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/pertemuan8pam/room/DatabaseSiswa.kt
@@ -1,4 +1,9 @@
 package com.example.pertemuan8pam.room
 
-class DatabaseSiswa {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
+    abstract fun siswaDao() : SiswaDao
 }
