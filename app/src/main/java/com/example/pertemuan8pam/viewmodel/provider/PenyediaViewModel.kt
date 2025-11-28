b407e8d9552d37f6d7bdf0da69a2b7318d3df13a
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 19:17:33 +0700

    membuat object untuk penyedia view model

--- app/src/main/java/com/example/pertemuan8pam/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan8pam/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,19 @@
 package com.example.pertemuan8pam.viewmodel.provider
 
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.sqlite.db.SupportSQLiteOpenHelper
+import com.example.pertemuan8pam.viewmodel.EntryViewModel
+import com.example.pertemuan8pam.viewmodel.HomeViewModel
+import kotlinx.coroutines.channels.Channel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
