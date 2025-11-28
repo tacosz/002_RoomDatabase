9fc7531c8fa3ecc0ed94c4c01b11d75eadd858f6
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 19:20:23 +0700

    penyedia ekstensi query untuk objek application dan mengembalikan sebuah instance dari aplikasi siswa

--- app/src/main/java/com/example/pertemuan8pam/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan8pam/viewmodel/provider/PenyediaViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.pertemuan8pam.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import androidx.sqlite.db.SupportSQLiteOpenHelper
+import com.example.pertemuan8pam.repositori.AplikasiSiswa
 import com.example.pertemuan8pam.viewmodel.EntryViewModel
 import com.example.pertemuan8pam.viewmodel.HomeViewModel
 import kotlinx.coroutines.channels.Channel
@@ -17,3 +19,7 @@ object PenyediaViewModel {
         }
     }
 }
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiSiswa)
