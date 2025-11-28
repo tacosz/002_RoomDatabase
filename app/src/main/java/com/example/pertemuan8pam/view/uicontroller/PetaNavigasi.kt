cd330096eccb4adbe71c2f8b11fa8c102af56943
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 19:48:13 +0700

    membuat fungsi unt host navigasi

--- app/src/main/java/com/example/pertemuan8pam/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan8pam/view/uicontroller/PetaNavigasi.kt
@@ -1,12 +1,36 @@
 package com.example.pertemuan8pam.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan8pam.view.route.DestinasiEntry
+import com.example.pertemuan8pam.view.route.DestinasiHome
+import androidx.navigation.compose.composable
 
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController(),
              modifier: Modifier){
     HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        }
+    }
 }
