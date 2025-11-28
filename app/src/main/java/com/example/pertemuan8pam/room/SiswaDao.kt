98c3c4aa59d362e6c18e487cb7cce3194c7c1ed4
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 15:21:38 +0700

    membuat dao dan deklarasi dungsi unt get all siswa

--- app/src/main/java/com/example/pertemuan8pam/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan8pam/room/SiswaDao.kt
@@ -1,4 +1,11 @@
 package com.example.pertemuan8pam.room
 
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface SiswaDao {
+    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
 }
