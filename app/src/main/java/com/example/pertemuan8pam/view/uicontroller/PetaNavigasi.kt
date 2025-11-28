a9b7158506f5dd75ed30e4d64d4521e0694f02cb
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-28 19:41:14 +0700

    fungsi unt siswa app

--- app/src/main/java/com/example/pertemuan8pam/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan8pam/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,12 @@
 package com.example.pertemuan8pam.view.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(),
+             modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
