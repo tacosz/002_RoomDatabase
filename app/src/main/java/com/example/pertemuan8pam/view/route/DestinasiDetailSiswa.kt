646d15f26301071d69b12af8afffe8fd199248f5
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-05 15:10:31 +0700

    definisi menimpa method pada destinasi detail siswa

---
+++ app/src/main/java/com/example/pertemuan8pam/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan8pam.view.route
+
+import com.example.pertemuan8pam.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
