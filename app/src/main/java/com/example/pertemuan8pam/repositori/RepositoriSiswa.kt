d398e0cf218019af288e2967702a897a57c7c446
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-05 14:58:36 +0700

    fungsi jeda untuk menghapus siswa

--- app/src/main/java/com/example/pertemuan8pam/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan8pam/repositori/RepositoriSiswa.kt
@@ -18,5 +18,6 @@ class OfflineRepositoriSiswa(
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 
 }
