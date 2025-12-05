e75e7b4d8de8c067f4f4bc2421c012a404fc0109
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-05 15:13:20 +0700

    membuat class detail view model berisi handle penyimpanan dan variable private repositori siswa

---
+++ app/src/main/java/com/example/pertemuan8pam/viewmodel/DetailViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.pertemuan8pam.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8pam.repositori.RepositoriSiswa
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+): ViewModel(){
+
+}
