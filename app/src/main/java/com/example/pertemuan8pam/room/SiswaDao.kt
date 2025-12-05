db7ecccddb22c07c4d3852ff51ec4fd12971f645
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-05 14:44:06 +0700

    fungsi untuk hapus siswa

--- app/src/main/java/com/example/pertemuan8pam/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan8pam/room/SiswaDao.kt
@@ -16,4 +16,9 @@ interface SiswaDao {
 
     @Query(value = "SELECT * from tblSiswa WHERE id = :id")
     fun getSiswa(id: Int): Flow<Siswa>
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
+
+
 }
