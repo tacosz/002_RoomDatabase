0878d4c303502b4a34194a244e3c1e354dc27cc4
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-05 15:18:31 +0700

    menambahkan variabel private untuk id siswa ui detail state delete siswa dan companion object

--- app/src/main/java/com/example/pertemuan8pam/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan8pam/viewmodel/DetailViewModel.kt
@@ -3,10 +3,34 @@ package com.example.pertemuan8pam.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.pertemuan8pam.repositori.RepositoriSiswa
-
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
 class DetailViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoriSiswa: RepositoriSiswa
 ): ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DetailViewModel.itemIdArg])
 
+    val uiDetailState: StateFlow<DetailSiswaUiState> =
+        repositoriSiswa.getSiswaStream(idSiswa)
+            .filterNotNull()
+            .map {
+                DetailSiswaUiState(detailSiswa = it.toDetailSiswa())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailSiswaUiState()
+            )
+    suspend fun deleteSiswa(){
+        repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
+    }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
+
