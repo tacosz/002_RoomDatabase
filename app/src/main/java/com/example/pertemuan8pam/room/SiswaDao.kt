6f0d491b16a404a3c5a182047fe6c304671c8c9e
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-05 14:49:03 +0700

    import untuk update delete

--- app/src/main/java/com/example/pertemuan8pam/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan8pam/room/SiswaDao.kt
@@ -4,6 +4,8 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Delete
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
