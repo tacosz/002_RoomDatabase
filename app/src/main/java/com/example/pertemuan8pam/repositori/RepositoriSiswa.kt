b31b4ab531a470dbd6a8c89751d2a4b49488b233
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-05 14:54:43 +0700

    fungsi unt mengambil data siswa berdasarkan id

--- app/src/main/java/com/example/pertemuan8pam/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan8pam/repositori/RepositoriSiswa.kt
@@ -17,4 +17,6 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa{
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+
 }
